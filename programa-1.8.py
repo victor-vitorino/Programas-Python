c545700e3460c483b9ce9f415dd996e08feab6cc
Victor Augusto Vitorino Alves de Andrade <vitoralgustosp@hotmail.com>
2022-12-13 14:29:08 -0300

    Add files via upload

---
+++ programa-1.8.py
@@ -0,0 +1,12 @@
+a = int(input('Entre com o número: '))
+div = 0
+for x in range(1, a+1):
+    resto = a % x
+    print(x, resto)
+    if resto ==0:
+        div += 1
+
+if div ==2:
+    print('número {} é primo' .format(a))
+else:
+    print('número {} não é primo' .format(a))
